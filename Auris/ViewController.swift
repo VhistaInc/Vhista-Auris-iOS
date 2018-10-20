62647c3d0aa06469cae21eb40e6ef9b7e85a3a79
David Cruz <juandavidcruzs@gmail.com>
2018-10-19 23:49:46 -0500

    First Commit 🇨🇴😊

---
+++ Auris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auris
+//
+//  Created by Juan David Cruz Serrano on 10/19/18.
+//  Copyright © 2018 Juan David Cruz Serrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Auris' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Auris
+  pod 'Firebase/Core'
+  
+  # UI Pods for Auris
+  pod 'FlagKit'
+
+end
